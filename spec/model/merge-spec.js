11f93730f9e828367ab4c60a14f2bb9609b8f7a6
Ash Wilson <smashwilson@gmail.com>
2016-04-30 21:24:08 -0400

    More merge specs.

--- spec/model/merge-spec.js
+++ spec/model/merge-spec.js
@@ -4,25 +4,45 @@ import Switchboard from '../../lib/model/switchboard'
 import Merge, {ConflictingFile} from '../../lib/model/merge'
 
 describe('Merge', () => {
-  it('is read from a VCS context', () => {
-    const sb = new Switchboard()
+  let sb, fs, vcsContext, merge
 
-    const fs = [
+  beforeEach(() => {
+    sb = new Switchboard()
+
+    fs = [
       new ConflictingFile('lib/aaa.c', 'both modified'),
       new ConflictingFile('lib/bbb.c', 'both modified'),
       new ConflictingFile('lib/ccc.c', 'both modified')
     ]
 
-    const vcsContext = {
+    vcsContext = {
       isRebasing: () => false,
       readConflicts: () => Promise.resolve(fs)
     }
 
-    waitsForPromise(() => Merge.read(sb, vcsContext).then((merge) => {
-      expect(merge.switchboard).toBe(sb)
-      expect(merge.vcsContext).toBe(vcsContext)
-      expect(merge.files).toBe(fs)
-      expect(merge.isRebase).toBe(false)
-    }));
+    waitsForPromise(() => Merge.read(sb, vcsContext).then((m) => merge = m))
+  })
+
+  it('is read from a VCS context', () => {
+    expect(merge.switchboard).toBe(sb)
+    expect(merge.vcsContext).toBe(vcsContext)
+    expect(merge.files).toBe(fs)
+    expect(merge.isRebase).toBe(false)
+  })
+
+  it('enumerates conflicting paths', () => {
+    const ps = merge.conflictingPaths()
+    expect(ps).toEqual(['lib/aaa.c', 'lib/bbb.c', 'lib/ccc.c'])
+  })
+
+  it('reports non-emptiness', () => {
+    expect(merge.isEmpty()).toBe(false)
+  })
+
+  it('reports emptiness', () => {
+    fs = []
+    waitsForPromise(() => Merge.read(sb, vcsContext).then((other) => {
+      expect(other.isEmpty()).toBe(true)
+    }))
   })
 })
